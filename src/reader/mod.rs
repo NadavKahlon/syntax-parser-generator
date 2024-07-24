b870adb3dfaab69cfc29ff11bb1b9d0ea0472316
Nadav Kahlon <nadavkahlon100@gmail.com>
2024-07-24 10:41:34 +0300

    Remove reader-wrapper functionality

--- src/reader/mod.rs
+++ src/reader/mod.rs
@@ -1,8 +1,5 @@
-use crate::reader::wrapper::WrapperReader;
-
 mod address_based;
 mod string_reader;
-mod wrapper;
 
 pub trait Reader<T> {
     fn read_next(&mut self) -> T;
@@ -10,11 +7,4 @@ pub trait Reader<T> {
     fn set_tail(&mut self);
     fn reset_to_tail(&mut self);  // The next item read is the one after tail
     fn get_sequence(&self) -> impl Iterator<Item=T>;
-    fn wrap<U, W>(self, wrapper_function: W) -> impl Reader<U>
-    where
-        W: Fn(T) -> U,
-        Self: Sized,
-    {
-        WrapperReader::new(self, wrapper_function)
-    }
 }
