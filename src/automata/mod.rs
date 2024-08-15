867c93ea113b8479c3968573d06830f821af53e7
Nadav Kahlon <nadavkahlon100@gmail.com>
2024-08-16 01:49:59 +0300

    Remove irrelevant IMPROVE comments

--- src/automata/mod.rs
+++ src/automata/mod.rs
@@ -1,5 +1,3 @@
-// IMPROVE: use trait bounds on u8, u16, u32, u64, usize, to replace all occurrences of u16 here
-// IMPROVE: we may wanna replace all the `as usize`
 // IMPROVE: add validation at various methods
 // IMPROVE: change the internal representation of DfaState and NfaState to maximize space locality
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-// IMPROVE: consider using tuple struct throughout
-
 mod automata;
 mod lex;
 mod reader;
